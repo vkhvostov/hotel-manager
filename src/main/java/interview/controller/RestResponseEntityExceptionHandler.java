4e26bd0bf31b46a7e8b72fccd0210a64f4671c35
vkhvostov <viktor.khvostov@gmail.com>
2018-10-03 23:25:35 +0200

    Exception handler for the rest controller

---
+++ src/main/java/interview/controller/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,22 @@
+package interview.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * Exception handler for producing meaningful status code and message in case of server side exception
+ */
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler
+        extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = { IllegalArgumentException.class })
+    protected ResponseEntity<Object> handleException(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+}
