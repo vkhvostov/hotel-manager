de84a739c7d2ef21030d54f2f0f64e04182b8200
vkhvostov <viktor.khvostov@gmail.com>
2018-10-03 21:19:21 +0200

    Skeleton for a new application

---
+++ src/main/java/interview/Application.java
@@ -0,0 +1,16 @@
+package interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created on 03.10.18
+ * TODO: Add comment
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
